aefa125a98266ce2369ee8da16d54c96fe7d1bfb
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-26 03:14:47 +0530

    Update zchatbot.py

--- nexichat/idchatbot/zchatbot.py
+++ nexichat/idchatbot/zchatbot.py
@@ -328,8 +328,8 @@ async def chatbot_response(client: Client, message: Message):
         bot_id = client.me.id
         chat_status = await status_db.find_one({"chat_id": chat_id, "bot_id": bot_id})
         
-        if (chat_status and chat_status.get("status") == "disabled") or (message.text and message.chat.type not in ["group", "supergroup"]):
-            return 
+        if chat_status and chat_status.get("status") == "disabled":
+            return
 
         
         if message.text and any(message.text.startswith(prefix) for prefix in ["!", "/", ".", "?", "@", "#"]):
