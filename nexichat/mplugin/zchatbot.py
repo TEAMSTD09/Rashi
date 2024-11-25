51545fa1a2233b82405d64a182e544dae5c58a06
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-25 23:26:55 +0530

    Update zchatbot.py

--- nexichat/mplugin/zchatbot.py
+++ nexichat/mplugin/zchatbot.py
@@ -216,7 +216,7 @@ async def typing_effect(client, message, translated_text):
         await asyncio.sleep(0.01)
         await reply.edit_text(part1 + part2 + part3)
     except Exception as e:
-        print(f"Error in typing_effect: {e}")
+        return
 
 @Client.on_message(filters.incoming)
 async def chatbot_response(client: Client, message: Message):
