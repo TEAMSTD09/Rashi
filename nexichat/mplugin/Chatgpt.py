f9e95cbd8ea99914277bf057b2e62a8d53aedb02
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-25 23:00:04 +0530

    Update Chatgpt.py

--- nexichat/mplugin/Chatgpt.py
+++ nexichat/mplugin/Chatgpt.py
@@ -8,23 +8,25 @@ conversation_cache = {}
 
 @Client.on_message((filters.command(["gemini", "ai", "ask", "chatgpt"]) | filters.text), group=7)
 async def gemini_handler(client, message):
-    user_id = message.from_user.id
     user_input = None
 
-    if message.text.startswith(f"@{client.me.username}"):
-        user_input = message.text.split(" ", 1)[1] if len(message.text.split(" ", 1)) > 1 else None
-    elif (
-        message.text.startswith(f"/gemini@{client.me.username}")
-        and len(message.text.split(" ", 1)) > 1
-    ):
-        user_input = message.text.split(" ", 1)[1]
-    elif message.reply_to_message and message.reply_to_message.text:
-        user_input = message.reply_to_message.text
-    elif len(message.command) > 1:
+    if message.text.startswith(("/", ".")) and len(message.command) > 1:
         user_input = " ".join(message.command[1:])
-
+    elif message.text.startswith(f"@{client.me.username}"):
+        user_input = message.text.split(" ", 1)[1] if len(message.text.split(" ", 1)) > 1 else None
+    elif message.reply_to_message and message.reply_to_message.text:
+        if message.text.startswith(("/", ".") + tuple([f"/{command}" for command in ["ai", "ask", "chatgpt"]])) or \
+           message.text.startswith(f"@{client.me.username}"):
+            user_input = message.reply_to_message.text
+    if message.reply_to_message and len(message.command) > 1:
+        user_input = " ".join(message.command[1:])
+    elif message.reply_to_message and message.text.startswith(f"@{client.me.username}"):
+        split_text = message.text.split(" ", 1)
+        if len(split_text) > 1:
+            user_input = split_text[1]
     if not user_input:
-        await message.reply_text("ᴇxᴀᴍᴘʟᴇ :- `/ask who is Narendra Modi` or `@chutiyapabot how are you`")
+        if message.text.startswith(("/", ".", f"@{client.me.username}")):
+            await message.reply_text(f"ᴇxᴀᴍᴘʟᴇ :- `/ask who are you baby` or `@{client.me.username} how are you`")
         return
 
     if user_id not in conversation_cache:
