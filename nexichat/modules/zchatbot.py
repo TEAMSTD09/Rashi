74301e8e97349ee95bb13631adc701a2bf94d024
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-26 13:52:31 +0530

    Update zchatbot.py

--- nexichat/modules/zchatbot.py
+++ nexichat/modules/zchatbot.py
@@ -228,7 +228,7 @@ async def typing_effect(client, message, translated_text):
         return
 
 
-@nexichat.on_message(filters.private, group=10)
+@nexichat.on_message(filters.private, group=14)
 async def chatbot_response(client: Client, message: Message):
     user_id = message.from_user.id
     chat_id = message.chat.id
@@ -347,7 +347,7 @@ async def handle_reply(message, reply_data, translated_text):
     except Exception as e:
         print(f"Error sending reply: {e}")
 
-@nexichat.on_message(filters.incoming & filters.group, group=13)
+@nexichat.on_message(filters.incoming & filters.group, group=15)
 async def chatbot_responsee(client: Client, message: Message):
     global blocklist, message_counts
     try:
