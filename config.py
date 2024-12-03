931ec919b5b053be4d01bc8d88181bebca2f64cc
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-12-03 11:14:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,10 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "7738353753:AAE_5o5lD8PtQOiKt-d28wg7rutbR8iXhqY")
 STRING1 = getenv("STRING_SESSION", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/THE-VIP-BOY-OP/VIP-CHATBOT")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://chutiyapabot:a@cluster0.heiwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(getenv("OWNER_ID", "7482534247"))
 SUPPORT_GRP = "TG_FRIENDSS"
 UPDATE_CHNL = "VIP_CREATORS"
 OWNER_USERNAME = "THE_VIP_BOY"
