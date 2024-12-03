94838951c2cb73fad48bc8e66811c49bbf4cb64e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-12-03 11:19:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7738353753:AAE_5o5lD8PtQOiKt-d28wg7rutbR8iXhqY")
+BOT_TOKEN = getenv("BOT_TOKEN", "6561481013:AAEV640dDQPty5Upkx-TH4To8ZjnJnkSp_c")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://chutiyapabot:a@cluster0.heiwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(getenv("OWNER_ID", "7482534247"))
