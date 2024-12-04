b0210b71ae91f79b2c4c7bc68114cd6c10c37220
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-12-05 00:25:12 +0530

    Update __main__.py

--- nexichat/__main__.py
+++ nexichat/__main__.py
@@ -11,7 +11,7 @@ from config import OWNER_ID
 from nexichat import LOGGER, nexichat, userbot, load_clone_owners
 from nexichat.modules import ALL_MODULES
 from nexichat.modules.Clone import restart_bots
-from nexichat.modules.Id_Clone import restart_idchatbots
+#from nexichat.modules.Id_Clone import restart_idchatbots
 
 async def anony_boot():
     try:
@@ -23,8 +23,8 @@ async def anony_boot():
     
         asyncio.create_task(restart_bots())
         print("Restarting all cloned botsbin background")
-        asyncio.create_task(restart_idchatbots())
-        print("Restarting all id chatbot in background")
+        #asyncio.create_task(restart_idchatbots())
+       # print("Restarting all id chatbot in background")
         await load_clone_owners()
         #await asyncio.sleep(5)
         
