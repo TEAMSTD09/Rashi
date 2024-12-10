90f6d39ef604c6608b58ab37ccb11fe7bfec37b8
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-12-11 03:07:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "6561481013:AAEV640dDQPty5Upkx-TH4To8ZjnJnkSp_c"
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://chutiyapabot:a@cluster0.heiwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(getenv("OWNER_ID", "7482534247"))
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "OK_WIN_PREDICTIONS"
-OWNER_USERNAME = "THE_VIP_BOY"
+SUPPORT_GRP = getenv("SUPPORT_GRP", "TG_FRIENDSS")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "OK_WIN_PREDICTIONS")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "THE_VIP_BOY")
 API = getenv("API", "https://chatwithai.codesearch.workers.dev/?chat=")
