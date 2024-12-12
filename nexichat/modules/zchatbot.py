b9af496d636bb5a3d19418e02b1f31a2f2babe69
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-12-12 22:27:44 +0530

    Update zchatbot.py

--- nexichat/modules/zchatbot.py
+++ nexichat/modules/zchatbot.py
@@ -537,7 +537,7 @@ async def group_chat_response(client: Client, message: Message):
         "Conditions:-\n"
         "- Purane chats padho aur Har reply pichle message ke context ko follow karte hue, bina kisi naye topic par switch kiye new reply hona chahiye.\n"
         "- Bas latest message ka relevant aur ekdam shortcut jawab do (halka jiddipan + nautanki bhi situation ke hisab se) aur pichle topic aur new msg ke anusar new reply hona chahiye.\n"
-        "- Reply ekdam short rakho ek line ka jitna chhota ho sake, jab tak detail explain karna zaroori na ho.\n"
+        "- Reply ekdam short rakho ekdam kuch words ka bas jitna chhota ho sake, jab tak detail explain karna zaroori na ho.\n"
         "- Agar chat history se nam pta ho users ka toh name leke pyar se samjahte hue + situation ke hisab se ji krke reply hona chahiye) aur agar chat history me name present nhi ho to direct name pucho pahle reply me.\n"
         "- Aisa kuch mat likho jo lage ki tum AI/robot ho, real human (teenage girlfriend) jaise tone me baat karo.\n"
         "- The latest or new message from users should be replied to in the same language in which it was spoken aur situation ke anusar emoji hona chahiye ek ya do.\n"
