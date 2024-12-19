7fd69479af47e42af8ec67b68acecaafc94305ab
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-12-19 07:08:41 +0530

    Update zchatbot.py

--- nexichat/modules/zchatbot.py
+++ nexichat/modules/zchatbot.py
@@ -340,14 +340,16 @@ async def chatbot_response(client: Client, message: Message):
                     result = json_response.get("data", "").strip()
 
                     if result:
-                        await client.send_chat_action(message.chat.id, ChatAction.TYPING)
+                        await client.send_chat_action(chat_id, ChatAction.TYPING)
                         asyncio.create_task(typing_effect(client, message, result))
-                        
-                        if len(result) <= 500 and len(user_input) <= 500:
+
+                        if result and user_input:
+                            result = result[0:400]
+                            user_input = user_input[0:400]
                             conversation_cache[chat_id][user_id].append((user_input, result))
                         if len(conversation_cache[chat_id][user_id]) > 15:
                             conversation_cache[chat_id][user_id].pop(0)
-                        
+
                         return
                 except requests.RequestException as e:
                     print(f"Error with AI response: {e}")
@@ -575,7 +577,9 @@ async def group_chat_response(client: Client, message: Message):
                     await client.send_chat_action(chat_id, ChatAction.TYPING)
                     asyncio.create_task(typing_effect(client, message, result))
 
-                    if len(result) <= 500 and len(user_input) <= 500:
+                    if result and user_input:
+                        result = result[0:400]
+                        user_input = user_input[0:400]
                         conversation_cache[chat_id][user_id].append((user_input, result))
                     if len(conversation_cache[chat_id][user_id]) > 15:
                         conversation_cache[chat_id][user_id].pop(0)
