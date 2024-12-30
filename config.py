eacb41cb72162ca027952c890103886684b0f659
TEAMSTD09 <110489709+TEAMSTD09@users.noreply.github.com>
2024-12-30 11:58:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "26064247"
 # -------------------------------------------------------------
 API_HASH = "c55cdb7652486811acfa038f4cda154f"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "6561481013:AAEV640dDQPty5Upkx-TH4To8ZjnJnkSp_c")
+BOT_TOKEN = getenv("BOT_TOKEN", "7286765919:AAEKu26F5YGTrPn-hvRXlqXu8hZy52HJSiQ")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://chutiyapabot:a@cluster0.heiwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(getenv("OWNER_ID", "7482534247"))
